e37caad8ac58230d262013701cb3c51fe7c8049a
Alex Balgavy <alexander.balgavy@spaceapplications.com>
2023-12-22 23:55:32 +0100

    Key bindings

--- config.mk
+++ config.mk
@@ -13,7 +13,7 @@ X11LIB = /usr/X11R6/lib
 PKG_CONFIG = pkg-config
 
 # includes and libs
-INCS = -I$(X11INC) \
+INCS = -I/usr/local/include -I$(X11INC) \
        `$(PKG_CONFIG) --cflags fontconfig` \
        `$(PKG_CONFIG) --cflags freetype2`
 LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
